5e3a51d589b8f4173de0dd8403744bfc0bcccc0c
Leonardo Paz vargas <leonardo.paz@entelgy.com>
2024-02-28 16:12:00 -0500

    modal message

--- src/js/main.js
+++ src/js/main.js
@@ -765,6 +765,14 @@ function closeSearchBar(){
   });
 }
 
+function openModal() {
+  modal.classList.add('visible');
+}
+
+function closeModal() {
+  modal.classList.remove('visible');
+}
+
 //function resize
 window.addEventListener("resize", widthChangeCallback);
 widthChangeCallback();
@@ -801,6 +809,18 @@ if(document.getElementById('navigation')){
 }
 searchMobile();
 closeSearchBar();
+
+const openModalButtons = document.querySelectorAll('.open-modal'),
+      modal = document.querySelector('.modal'),
+      closeModalButtons = document.querySelectorAll('.close-modal');
+
+openModalButtons.forEach(openBtn => {
+  openBtn.addEventListener('click', openModal)
+});
+
+closeModalButtons.forEach(closeBtn => {
+  closeBtn.addEventListener('click', closeModal)
+});
 
 // invoke function DataLayer
 const elemDataLayer = document.querySelectorAll('*[class^="datalayer"]');
